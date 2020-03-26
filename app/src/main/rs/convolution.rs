9c25fb67454bc6439f357e5cbc39a299909e8188
Ibrahima Diallo <bessekoboy@gmail.com>
2020-03-26 19:18:03 +0100

    Merge branch 'master' of C:\Users\IBRAHIMA\projettec with conflicts.

---
+++ app/src/main/rs/convolution.rs
@@ -0,0 +1,14 @@
+#pragma version (1)
+#pragma rs java_package_name ( com . android . rssample )
+#pragma rs_fp_relaxed
+# include "rs_debug.rsh"
+
+int32 pixels;
+
+uchar4 RS_KERNEL convolution (uchar4 in , uint32_t x , uint32_t y ) {
+uchar4 out = in ;
+out . r = 255 - in . r ;
+out . g = 255 - in . g ;
+out . b = 255 - in . b ;
+return out ;
+}
